feb1659402ca684d9f94a87d36b5dd8ff247d303
akash <akashkumar@cdac.in>
2025-10-29 17:43:15 +0530

    removal of apigen from api.jsx

--- aipptgen/src/App.js
+++ aipptgen/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import AiChatPptApp from './APIGen/ApiGen';
+// import AiChatPptApp from './APIGen/ApiGen';
 import SlideGenerator from './APIGen/HomePage';
 
 function App() {
